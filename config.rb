3c13911d8abb59524db1bd763f6876fd7a0334f9
Wayne Graham <wayne.graham@virginia.edu>
2011-08-18 14:08:05 -0400

    Added 960gs to sass and modified layout

--- config.rb
+++ config.rb
@@ -1,10 +1,12 @@
+require 'ninesixty' # 960 plugin
+
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "_sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
-# output_style  = :expanded
-output_style  = :compressed
+output_style  = :expanded
+#output_style  = :compressed
 line_comments = false
 http_fonts_path = "http://static.scholarslab.org/fonts/MuseoSlab/"
 
