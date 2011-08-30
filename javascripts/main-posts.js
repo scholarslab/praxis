d872916591fc9c67938e1093849ec2192201b80b
Wayne Graham <wayne.graham@virginia.edu>
2011-08-30 06:10:18 -0400

    Limit posts on main page to 5

--- javascripts/main-posts.js
+++ javascripts/main-posts.js
@@ -1,7 +1,7 @@
 var API_KEY = 'ABQIAAAAioVG4cScdJ2Csnny8hxlzRQ0k7LmwNWN1KPzdPryee6GpBC_0hRQjOPG8jfV_sgUu5ehe3wi4vZkdA';
 $(document).ready(function () {
   $('#blog').rssfeed('http://www.scholarslab.org/category/praxis-program/feed/', {
-    limit: 10,
+    limit: 5,
     titletag: 'h1',
     snipett: true,
     date: false,
