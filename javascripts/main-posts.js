4517ea3de41cf23b0ddae099d0038739b9ac214c
Jeremy Boggs <jeremy@clioweb.org>
2013-04-16 10:24:48 -0400

    Updates feed URL for Praxis posts.

--- javascripts/main-posts.js
+++ javascripts/main-posts.js
@@ -1,6 +1,6 @@
 var API_KEY = 'ABQIAAAAioVG4cScdJ2Csnny8hxlzRQ0k7LmwNWN1KPzdPryee6GpBC_0hRQjOPG8jfV_sgUu5ehe3wi4vZkdA';
 $(document).ready(function () {
-  $('#blog').rssfeed('http://www.scholarslab.org/category/praxis-program/feed/', {
+  $('#blog').rssfeed('http://www.scholarslab.org/tag/praxis-program/feed/', {
     limit: 5,
     titletag: 'h1',
     snipett: true,
