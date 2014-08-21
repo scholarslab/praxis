d9ebe9e9792b403b2c71dbc754acff0619f342d0
Jeremy Boggs <jeremy@clioweb.org>
2014-08-21 16:56:42 -0400

    Merges some scripts.

---
+++ javascripts/scripts.js
@@ -0,0 +1,25 @@
+// Responsive-nav.js
+var nav = responsiveNav(
+    "header nav",
+    {
+        label: "☰ Menu"
+    }
+);
+
+// Headroom.js
+var myElement = document.querySelector("#banner");
+var headroom  = new Headroom(myElement);
+headroom.init();
+
+// Konami
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+$(document).keydown(function(e) {
+    kkeys.push( e.keyCode );
+    if ( kkeys.toString().indexOf( konami ) >= 0 ){
+        $(document).unbind('keydown',arguments.callee);
+        $.getScript('//www.cornify.com/js/cornify.js',function(){
+            cornify_add();
+            $(document).keydown(cornify_add);
+        });
+    }
+});
